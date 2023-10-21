45b4a41f3e802b9d056ee722070ae6566003f2cb
azakiee <148317913+azakiee@users.noreply.github.com>
2023-10-21 16:51:21 +0300

    Add files via upload

---
+++ Class2.py
@@ -0,0 +1,21 @@
+from CLASS import Student
+
+
+class Grade8(Student):
+    def __init__(self, name, age, grade, power):
+        super().__init__(name, age, 8, power)
+
+
+class Grade9(Student):
+    def __init__(self, name, age, grade, power):
+        super().__init__(name, age, 9, power)
+
+
+class Grade10(Student):
+    def __init__(self, name, age, grade, power):
+        super().__init__(name, age, 10, power)
+
+
+class Grade11(Student):
+    def __init__(self, name, age, grade, power):
+        super().__init__(name, age, 11, power)
