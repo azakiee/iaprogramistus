023b9b498d1a31397559df031ebf0a9f2f1e2ea5
azakiee <148317913+azakiee@users.noreply.github.com>
2023-11-17 16:11:55 +0300

    Полностью рабочий диалог. Имя, фамилия, дата

--- FSM.py
+++ FSM.py
@@ -17,43 +17,111 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 
+WAIT_NAME, WAIT_SURNAME, WAIT_BIRTHDAY = range(3)
+
+
 def ask_name(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
-    name = update.message.from_user.name
-    text = update.message.text
-    logger.info(f'{user_id=} Bызвaл функцию ask_name')
-    text = f'Твоё имя - {text}'
+    username = update.message.from_user.username
+    logger.info(f'{username=} {user_id=} вызвал функцию ask_name')
+    answer = [
+        f'Привет!',
+        f'Назови свое имя'
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer, reply_markup=ReplyKeyboardRemove())
+
+    return WAIT_NAME
 
 
 def get_name(update: Update, context: CallbackContext):
-    pass
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    text = update.message.text
+    logger.info(f'{username=} {user_id=} вызвал функцию get_name')
+    answer = [
+        f'Твое имя - {text}'
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer)
+
+    return ask_surname(update, context)
 
 
 def ask_surname(update: Update, context: CallbackContext):
-    pass
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    logger.info(f'{username=} {user_id=} вызвал функцию ask_surname')
+    answer = [
+        f'Назови свою фамилию'
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer)
+
+    return WAIT_SURNAME
 
 
 def get_surname(update: Update, context: CallbackContext):
-    pass
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    text = update.message.text
+    logger.info(f'{username=} {user_id=} вызвал функцию get_surname')
+    answer = [
+        f'Твоя фамилия - {text}'
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer)
+
+    return ask_birthday(update, context)
 
 
 def ask_birthday(update: Update, context: CallbackContext):
-    pass
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    logger.info(f'{username=} {user_id=} вызвал функцию ask_birthday')
+    answer = [
+        f'Напиши свою дату рождения'
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer)
+
+    return WAIT_BIRTHDAY
 
 
 def get_birthday(update: Update, context: CallbackContext):
-    pass
+    user_id = update.message.from_user.id
+    username = update.message.from_user.username
+    text = update.message.text
+    logger.info(f'{username=} {user_id=} вызвал функцию get_birthday')
+    answer = [
+        f'Твоя дата рождения - {text}'
+    ]
+    answer = '\n'.join(answer)
+    update.message.reply_text(answer)
+
+    return register(update, context)
 
 
 def register(update: Update, context: CallbackContext):
     user_id = update.message.from_user.id
-    name = update.message.from_user.name
-    logger.info(f'{user_id=} Bызвaл функцию register')
+    username = update.message.from_user.username
+    logger.info(f'{username=} {user_id=} вызвал функцию register')
     answer = [
-        f'Привет!'
-        f'Зарегистрировал тебя!'
+        f'Привет!',
+        f'Ты зареган!'
     ]
     answer = '\n'.join(answer)
     update.message.reply_text(answer)
 
     return ConversationHandler.END
+
+
+register_handler = ConversationHandler(
+    entry_points=[CommandHandler('register', ask_name)],
+    states={
+        WAIT_NAME: [MessageHandler(Filters.text, get_name)],
+        WAIT_SURNAME: [MessageHandler(Filters.text, get_surname)],
+        WAIT_BIRTHDAY: [MessageHandler(Filters.text, get_birthday)],
+    },
+    fallbacks=[]
+)
